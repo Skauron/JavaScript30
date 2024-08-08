627882cf11df9628921f8db89f035063a38d387c
Momo <43181268+Skauron@users.noreply.github.com>
2024-08-08 10:38:11 -0500

    Fixed for mobile

--- script.js
+++ script.js
@@ -13,36 +13,56 @@ let LastY = 0;
 let hue = 0;
 let direction = true;
 
-function draw(e){
-  if(!isDrawing) return;
-  
+function draw(e) {
+  if (!isDrawing) return;
+
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
-  ctx.moveTo(LastX,LastY);
-  ctx.lineTo(e.offsetX,e.offsetY);
+  ctx.moveTo(LastX, LastY);
+
+  //* PC O MOBILE
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    ctx.lineTo(e.touches[0].clientX, e.touches[0].clientY);
+  } else {
+    ctx.lineTo(e.offsetX, e.offsetY);
+  }
+  
   ctx.stroke();
-  [LastX, LastY] = [e.offsetX, e.offsetY];
+  //* PC O MOBILE
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    [LastX, LastY] = [e.touches[0].clientX, e.touches[0].clientY];
+  } else {
+    [LastX, LastY] = [e.offsetX, e.offsetY];
+  }
 
   hue++;
-  if(hue >= 360){
+  if (hue >= 360) {
     hue = 0;
   }
-  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
     direction = !direction;
   }
-  if(direction){
+  if (direction) {
     ctx.lineWidth++;
-  }else{
+  } else {
     ctx.lineWidth--;
   }
-
 }
 
+//* PC
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
   [LastX, LastY] = [e.offsetX, e.offsetY];
 });
-
 canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", () => isDrawing = false);
-canvas.addEventListener("mouseout", () => isDrawing = false);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+//*MOBILE
+canvas.addEventListener("touchstart", (e) => {
+  isDrawing = true;
+  [LastX, LastY] = [e.touches[0].clientX, e.touches[0].clientY];
+});
+canvas.addEventListener("touchmove", draw);
+canvas.addEventListener("touchcancel", () => (isDrawing = false));
+canvas.addEventListener("touchend", () => (isDrawing = false));
