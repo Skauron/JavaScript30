99449d56b0ee5d9b73feca8a6b62bd197c17c7d1
Momo <43181268+Skauron@users.noreply.github.com>
2024-08-16 09:06:25 -0500

    Commit

--- script.js
+++ script.js
@@ -1,62 +1,19 @@
-const addItems = document.querySelector(".add-items");
-const itemsList = document.querySelector(".plates");
-const items = JSON.parse(localStorage.getItem("items")) || [];
-const buttonsList = document.querySelectorAll(".button");
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 100; //100px
 
-function addItem(e) {
-  e.preventDefault();
-  const text = this.querySelector("[name=item]").value;
-  const item = {
-    text,
-    donce: false,
-  };
-  items.push(item);
-  populateList(items, itemsList);
-  localStorage.setItem("items", JSON.stringify(items));
-  this.reset();
-}
-
-function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates
-    .map((plate, i) => {
-      return `
-      <li>
-        <input type="checkbox" data-index=${i} id="item${i}" ${
-        plate.done ? "checked" : ""
-      }/>
-        <label for="item${i}">${plate.text}</label>
-      </li>
-    `;
-    })
-    .join("");
-}
-
-function toggleDone(e) {
-  if (!e.target.matches("input")) return; //Skip this unless it's an input
-  items[e.target.dataset.index].done = !items[e.target.dataset.index].done;
-  localStorage.setItem("items", JSON.stringify(items));
-  populateList(items, itemsList);
-}
-
-function buttonFunction(e) {
-  switch (e.target.name) {
-    case "Clear":
-      items.splice(0, items.length);
-      break;
-    case "Uncheck":
-      items.map((plate) => plate.done = false);
-      break;
-    case "Check":
-      items.map((plate) => plate.done = true);
-      break;
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+  if(this !== e.target){
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
   }
-  localStorage.setItem("items", JSON.stringify(items));
-  populateList(items, itemsList);
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,255,255,0.7)`;
 }
 
-addItems.addEventListener("submit", addItem);
-itemsList.addEventListener("click", toggleDone);
-buttonsList.forEach((button) => {
-  button.addEventListener("click", buttonFunction);
-});
-populateList(items, itemsList);
+hero.addEventListener("mousemove", shadow);
