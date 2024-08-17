556c12240f82d9fcb9d9ba040eaa41d3a8b693eb
Momo <43181268+Skauron@users.noreply.github.com>
2024-08-17 17:42:28 -0500

    Comit

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-const hero = document.querySelector(".hero");
-const text = hero.querySelector("h1");
-const walk = 100; //100px
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
 
-function shadow(e) {
-  const { offsetWidth: width, offsetHeight: height } = hero;
-  let { offsetX: x, offsetY: y } = e;
-  if(this !== e.target){
-    x = x + e.target.offsetLeft;
-    y = y + e.target.offsetTop;
-  }
-
-  const xWalk = Math.round((x / width * walk) - (walk / 2));
-  const yWalk = Math.round((y / height * walk) - (walk / 2));
-
-  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,255,255,0.7)`;
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, "").trim();
 }
 
-hero.addEventListener("mousemove", shadow);
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join("");
