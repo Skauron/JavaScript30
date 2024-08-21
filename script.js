d4524ed99635af99112529c3410aefdf2149e019
Momo <43181268+Skauron@users.noreply.github.com>
2024-08-21 17:01:33 -0500

    Commit

--- script.js
+++ script.js
@@ -1,106 +1,27 @@
-const video = document.querySelector(".player");
-const canvas = document.querySelector(".photo");
-const ctx = canvas.getContext("2d");
-const strip = document.querySelector(".strip");
-const snap = document.querySelector(".snap");
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-function getVideo() {
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((localmediaStream) => {
-      //console.log(localmediaStream);
-      video.srcObject = localmediaStream;
-      video.play();
-    })
-    .catch((err) => {
-      console.error("Hey you need to allow  webcam for this", err);
-    });
-}
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
 
-function paintToCanvas() {
-  const width = video.videoWidth;
-  const height = video.videoHeight;
-  canvas.width = width;
-  canvas.height = height;
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
 
-  return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height);
-    //Take th pixels out
-    let pixels = ctx.getImageData(0, 0, width, height);
+recognition.addEventListener("result", e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
 
-    //mess with them
-    //pixels = redEffect(pixels);
-
-    pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.1;
-
-    //pixels = greeScreen(pixels);
-    //put them back
-    ctx.putImageData(pixels, 0, 0);
-  }, 16);
-}
-
-function takePhoto() {
-  //Played the sound
-  snap.currentTime = 0;
-  snap.play();
-
-  //Take teh data out of the canvas
-  const data = canvas.toDataURL("image/jpeg");
-  const link = document.createElement("a");
-  link.href = data;
-  link.setAttribute("download", "handsome");
-  link.innerHTML = `<img src="${data}" alt="Handsome" />`;
-  strip.insertBefore(link, strip.firstChild);
-}
-
-function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
-  }
-  return pixels;
-}
-
-function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 150] = pixels.data[i + 2]; // BLUE
-  }
-  return pixels;
-}
-
-function greeScreen(pixels) {
-  const levels = {};
-
-  document.querySelectorAll(".rgb input").forEach((input) => {
-    levels[input.name] = input.value;
-  });
-
-  for (i = 0; i < pixels.data.length; i = i + 4) {
-    red = pixels.data[i + 0];
-    green = pixels.data[i + 1];
-    blue = pixels.data[i + 2];
-    alpha = pixels.data[i + 3];
-
-    if (
-      red >= levels.rmin &&
-      green >= levels.gmin &&
-      blue >= levels.bmin &&
-      red <= levels.rmax &&
-      green <= levels.gmax &&
-      blue <= levels.bmax
-    ) {
-      // take it out!
-      pixels.data[i + 3] = 0;
+    p.textContent = transcript;
+    if(e.results[0].isFinal){
+      p = document.createElement("p");
+      words.appendChild(p);
     }
-  }
+    
+});
 
-  return pixels;
-}
+recognition.addEventListener("end", recognition.start);
 
-getVideo();
-
-video.addEventListener("canplay", paintToCanvas);
+recognition.start();
