6a27b4d19d60547d53ab03ad9f73ebcdb6599e84
Momo <43181268+Skauron@users.noreply.github.com>
2024-08-22 09:46:46 -0500

    Commit

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const arrow = document.querySelector(".arrow");
-const speed = document.querySelector(".speed-value");
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
 
-navigator.geolocation.watchPosition(
-  (data) => {
-    speed.textContent = data.coords.speed;
-    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-  },
-  (err) => {
-    alert("Hey, you gotta allow that to happen!!!");
+highlight.classList.add("highlight");
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
   }
-);
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
